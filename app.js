24feab54f384f2c02919cf1225b9449dbd032c2e
Hikmet Meskic <hikmet.meskic2018@size.ba>
2021-12-17 00:05:02 +0100

    Clear images after submiting

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const form = document.querySelector('#searchForm');
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const searchTerm = form.elements.query.value;
-    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
-    showImages(res.data);
-})
 
+let imageCounter = 0;
 function showImages(shows) {
     for (let result of shows) {
         if (result.show.image) {
             const img = document.createElement('IMG');
             img.src = result.show.image.medium;
             document.body.append(img);
+            ++imageCounter;
         }
     }
 }
+
+function clearImages() {
+    for (let i = 0; i < imageCounter; i++) {
+        document.body.querySelector(img);
+    }
+}
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    clearImages();
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm } };
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    showImages(res.data);
+})
