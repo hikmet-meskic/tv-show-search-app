db5f80b1f27f7e95f1cf292ff0ca17a50a1dc6ba
Hikmet Meskic <hikmet.meskic2018@size.ba>
2021-12-17 01:11:15 +0100

    Make page responsive

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const form = document.querySelector('#searchForm');
+const row = document.querySelector('.row');
 
-let imageCounter = 0;
 function showImages(shows) {
     for (let result of shows) {
         if (result.show.image) {
             const img = document.createElement('IMG');
             img.src = result.show.image.medium;
-            document.body.append(img);
-            ++imageCounter;
+            img.className = 'column';
+            row.append(img);
         }
     }
 }
 
 function clearImages() {
-    for (let i = 0; i < imageCounter; i++) {
-        document.body.querySelector(img);
+    const imgArray = document.querySelectorAll('.column');
+    for (img of imgArray) {
+        img.remove();
     }
 }
 
